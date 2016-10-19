a890b6998c8aa1893cb3ceb707e55f3ff88ccd25
Mithlesh Jha <kumarmithlesh25@gmail.com>
2016-10-19 23:24:00 +0530

    Initial Commit

---
+++ PrintLabelTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  PrintLabelTest
+//
+//  Created by Mithlesh Jha on 19/10/16.
+//  Copyright © 2016 Mithlesh Jha. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ PrintLabelTest/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  PrintLabelTest
+//
+//  Created by Mithlesh Jha on 19/10/16.
+//  Copyright © 2016 Mithlesh Jha. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
